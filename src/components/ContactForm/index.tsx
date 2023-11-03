b5c0f03a0a18509e56ffff5c4fa2fce2d2299362
haothien1103 <tuhaothien51@gmail.com>
2023-11-03 12:43:15 +0700

    Nhan

---
+++ src/components/ContactForm/index.tsx
@@ -0,0 +1,76 @@
+import { Row, Col } from "antd";
+import { withTranslation } from "react-i18next";
+import { Slide, Zoom } from "react-awesome-reveal";
+import { ContactProps, ValidationTypeProps } from "./types";
+import { useForm } from "../../common/utils/useForm";
+import validate from "../../common/utils/validationRules";
+import { Button } from "../../common/Button";
+import Block from "../Block";
+import Input from "../../common/Input";
+import TextArea from "../../common/TextArea";
+import { ContactContainer, FormGroup, Span, ButtonContainer } from "./styles";
+
+const Contact = ({ title, content, id, t }: ContactProps) => {
+  const { values, errors, handleChange, handleSubmit } = useForm(validate);
+
+  const ValidationType = ({ type }: ValidationTypeProps) => {
+    const ErrorMessage = errors[type as keyof typeof errors];
+    return (
+      <Zoom direction="left">
+        <Span>{ErrorMessage}</Span>
+      </Zoom>
+    );
+  };
+
+  return (
+    <ContactContainer id={id}>
+      <Row justify="space-between" align="middle">
+        <Col lg={12} md={11} sm={24} xs={24}>
+          <Slide direction="left" triggerOnce>
+            <Block title={title} content={content} />
+          </Slide>
+        </Col>
+        <Col lg={12} md={12} sm={24} xs={24}>
+          <Slide direction="right" triggerOnce>
+            <FormGroup autoComplete="off" onSubmit={handleSubmit}>
+              <Col span={24}>
+                <Input
+                  type="text"
+                  name="Họ & Tên"
+                  placeholder="Họ Và Tên"
+                  value={values.name || ""}
+                  onChange={handleChange}
+                />
+                <ValidationType type="name" />
+              </Col>
+              <Col span={24}>
+                <Input
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  value={values.email || ""}
+                  onChange={handleChange}
+                />
+                <ValidationType type="email" />
+              </Col>
+              <Col span={24}>
+                <TextArea
+                  placeholder="Hãy để lại lời nhắn"
+                  value={values.message || ""}
+                  name="Lời nhắn"
+                  onChange={handleChange}
+                />
+                <ValidationType type="message" />
+              </Col>
+              <ButtonContainer>
+                <Button name="submit">{t("Gửi")}</Button>
+              </ButtonContainer>
+            </FormGroup>
+          </Slide>
+        </Col>
+      </Row>
+    </ContactContainer>
+  );
+};
+
+export default withTranslation()(Contact);
